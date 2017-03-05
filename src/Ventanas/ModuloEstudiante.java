4477ab6f128f642fa97b36ae258ec43b8cd80cab
WilsonXicara <wilsonx192@gmail.com>
2017-03-05 12:19:12 -0600

    Creación de mi branch
    En esta rama se desarrollará el Módulo Estudiante (en el que se
    visualizará la información básica de los Estudiantes)

---
+++ src/Ventanas/ModuloEstudiante.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ventanas;
+
+/**
+ * En este módulo se presentará la información básica de los estudiantes. Dicha información consiste en:
+ * -> Información básica (nombres, apelliidos, dirección, código, etc.).
+ * -> Información básica de la persona encargada.
+ * -> Información de su grado y los cursos que lleva.
+ * @author Wilson Xicará.
+ */
+public class ModuloEstudiante {
+    
+}
